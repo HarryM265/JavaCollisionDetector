bfaba35608a0b862bdf9a539929f6b7965928b20
Harrison Miles <20990084@student.curtin.edu.au>
2022-06-03 18:21:39 +0800

    First Commit

---
+++ Main.java
@@ -0,0 +1,22 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Collision Detector");
+
+        Panel gamePanel = new Panel();
+        window.add(gamePanel);// Adds the game panel to the current window as a component
+
+        window.pack();// Sizes the window to the preferred size of its components
+
+        // Changed from .setLocation to .setLocationRelativeTo, making the window display at the centre of the screen
+        window.setLocationRelativeTo(null); // Window will be displayed at the centre of the screen
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
+}
---
+++ Panel.java
@@ -0,0 +1,150 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+import javax.swing.plaf.basic.BasicComboBoxUI;
+import javax.swing.plaf.basic.BasicComboBoxUI.KeyHandler;
+
+public class Panel extends JPanel implements Runnable {
+
+    final int screenWidth = 400;
+    final int screenHeight = 400;
+
+    final int halfScreenWidth = (int)(screenWidth/2);
+    final int halfScreenHeight = (int)(screenHeight/2);
+
+    //CentreRect dimensions
+    final int centreRectWidth = screenWidth/2;
+    final int centreRectHeight  = screenHeight/3;
+    final int halfCentreRectWidth = (int)(centreRectWidth/2);
+    final int halfCentreRectHeight = (int)(centreRectHeight/2);
+    //CentreRect location
+    int centreRectX = halfScreenWidth - halfCentreRectWidth;
+    int centreRectY = halfScreenHeight - halfCentreRectHeight;
+
+    //BouncingRect starting location
+    final int bouncingRectStartLoc = 5;
+    //BouncingRect dimensions
+    final int bouncingRectWidth  = screenWidth/20;
+    final int bouncingRectHeight = screenHeight/20;
+    //BouncingRect location
+    int bouncingRectX = bouncingRectStartLoc;
+    int bouncingRectY = bouncingRectStartLoc;
+    
+    int bouncingRectSpeedX = screenWidth/100;
+    int bouncingRectSpeedY = screenHeight/160;
+
+    //FPS
+    int fps = 60;
+
+    BasicComboBoxUI bComBoUI = new BasicComboBoxUI();
+    KeyHandler keyH = bComBoUI.new KeyHandler();
+    Thread gameThread;
+
+    //Set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    public Panel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));// Set the window size
+        this.setBackground(Color.GRAY);// Make the background black
+        this.setDoubleBuffered(true);// All drawing from this component will be performed on an off-screen painting buffer
+        this.addKeyListener(keyH);
+        this.setFocusable(true);// this game panel can be 'focused' to recieve key input
+
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        double drawInterval = 1000000000 / fps;
+        double delta = 0;
+        // 1,000,000,000 nano seconds = 1 second
+        long lastTime = System.nanoTime();// Returns the time since the current program was ran in nano seconds
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while (gameThread != null) {
+
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;// Find how much time has passed and divide it by the draw interval
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            //When delta reaches the draw interval, draw and update, then reset the delta
+            if (delta >= 1) {
+            // 1 UPDATE: update information such as character position
+            update();
+            // 2 DRAW: draw the screen with the updated information
+            repaint(); // Calls paintComponent
+            delta--;
+            drawCount++;
+            }
+
+            if (timer >= 1000000000) {
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
+        
+    }
+
+    public void update() {
+        //Add Movement Code
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // x-axis movement controller
+        //If I keep moving in my current X direction, will I collide with the centre rectangle?
+        if (bouncingRectX + bouncingRectWidth + bouncingRectSpeedX > centreRectX && 
+            bouncingRectX + bouncingRectSpeedX < centreRectX + centreRectWidth && 
+            bouncingRectY + bouncingRectHeight > centreRectY && 
+            bouncingRectY < centreRectY + centreRectHeight) {
+            bouncingRectSpeedX *= -1;
+        //Bounce off the left and right of the screen
+        } else if (bouncingRectX < 0 || bouncingRectX + bouncingRectWidth > screenWidth) {
+            bouncingRectSpeedX *= -1;
+        }
+
+        // y-axis movement controller
+        //If I keep moving in my current Y direction, will I collide with the centre rectangle?
+        if (bouncingRectX + bouncingRectWidth > centreRectX && 
+            bouncingRectX < centreRectX + centreRectWidth && 
+            bouncingRectY + bouncingRectHeight + bouncingRectSpeedY > centreRectY && 
+            bouncingRectY + bouncingRectSpeedY < centreRectY + centreRectHeight) {
+            bouncingRectSpeedY *= -1;
+        //Bounce off the top and bottom edges of the screen
+        } else if (bouncingRectY < 0 || bouncingRectY + bouncingRectHeight > screenHeight) {
+            bouncingRectSpeedY *= -1;
+        }
+
+        //Movement Code
+        bouncingRectX += bouncingRectSpeedX;
+        bouncingRectY += bouncingRectSpeedY;
+  
+        Graphics2D centreRect = (Graphics2D)g;
+        centreRect.setColor(Color.WHITE);
+
+        centreRect.fillRect(centreRectX, centreRectY, centreRectWidth, centreRectHeight);
+
+        Graphics2D bouncingRect = (Graphics2D)g;
+        bouncingRect.setColor(Color.BLACK);
+
+        bouncingRect.fillRect(bouncingRectX, bouncingRectY, bouncingRectWidth, bouncingRectHeight);
+
+        // disposal of this graphics context and stop using system resources
+        centreRect.dispose();
+        bouncingRect.dispose();
+    }
+}
